404e140c527af9491fd35fd7bef446a1d8acbc9d
Bjoey <87459440+wisnubjoey@users.noreply.github.com>
2024-05-20 09:54:59 +0800

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -53,11 +53,7 @@ export default function Navbar() {
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
+              <h1 className='text-xl font-bold text-black underline decoration-black hover:text-amber-500 transition duration-200'>K7</h1>
             </a>
             <button
               type="button"
